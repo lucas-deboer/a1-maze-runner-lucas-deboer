07cc9da97728d4ff5d7dd33766b7f674fe5fc8b7
Lucas DeBoer <ldeboer148@gmail.com>
2024-01-20 22:50:35 -0500

    feat: finished walking skeleton

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -1,44 +1,29 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.lang.module.Configuration;
-import java.nio.file.Path;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Options;
-//Parse exception
-public class Main {
 
+public class Main {
+    private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
+        logger.info("This is the business code.");
         // reads in the arguments that the user passed in to configure project
         Configuration config = Configuration.load(args);
 
-        // converts provided text file to a usable format
-        Maze theMaze = new Maze(config.file());
+        // converts provided Maze text file to a usable format
+        Maze theMaze = new Maze(config);
 
-        //find the starting point of the maze
-        theMaze.findStart();
+        //find the left and right openings in the maze
+        theMaze.findLeftHole();
+        theMaze.findRightHole();
 
-        // find a solution to the maze
-
-        //if no -p flag
-        MazePath path = new MazePath.findPath(theMaze);
-
-        //else if -p flag
-        //Read in user path
-        MazePath path = new MazePath(config.path());
-        //defactorize the path
-        path.defactorize();
+        //find solution to the maze
+        MazePath path = new MazePath();
+        path.findPath(theMaze);
 
         //verify the user input path
-        MazePath.verfiy(theMaze, path);
+        path.verifyPath(theMaze,config);
 
         // final outputs (i.e. either the correct path or if the provided path is valid
         path.export();
---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazePath.java
@@ -0,0 +1,14 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+public class MazePath {
+    //
+    public void findPath(Maze theMaze) {
+    }//find a solution to the maze
+
+    public void defactorize(Configuration config){
+    }//ensure the user input path is in canonical form
+    public void verifyPath(Maze theMaze, Configuration config) {
+    }//check if the user input path is valid
+    public void export() {
+    }//Display the end results (i.e. the valid path or if path is valid)
+}
