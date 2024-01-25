ede7234050c7527fe0da2beb6064e24bc980cefd
Lucas DeBoer <ldeboer148@gmail.com>
2024-01-25 17:17:18 -0500

    bug fix: switched the direction vectors in changeDirection. (close #20)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Direction.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Direction.java
@@ -3,7 +3,7 @@ package ca.mcmaster.se2aa4.mazerunner;
 import java.util.Arrays;
 
 public class Direction {
-    //Let the vectors be represented as [x,y], with possible values -1,1. 1 == up/right, -1 == left/down
+    //Let the vectors be represented as [x,y], with possible values -1,1. 1 == down/right, -1 == left/up
     public static int[] changeDirection(int[] currDirection, char move){
         if (Arrays.equals(currDirection,new int[]{0,1})){//faced North
             if(move == 'R') {
@@ -21,16 +21,16 @@ public class Direction {
             currDirection[1] = 0;
         }else if (Arrays.equals(currDirection,new int[]{1,0})) {//faced east
             if(move == 'R') {
-                currDirection[1] = -1;
-            }else{
                 currDirection[1] = 1;
+            }else{
+                currDirection[1] = -1;
             }
             currDirection[0] = 0;
         }else{//faced west
             if(move == 'R') {
-                currDirection[1] = 1;
-            }else{
                 currDirection[1] = -1;
+            }else{
+                currDirection[1] = 1;
             }
             currDirection[0] = 0;
         }
