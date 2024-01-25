9218721305bef608eb6b0b521cd2b19056dd1824
Lucas DeBoer <ldeboer148@gmail.com>
2024-01-25 18:21:48 -0500

    feat: added method getDirection solve using right hand rule. (closes #13)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Direction.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Direction.java
@@ -1,6 +1,7 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Direction {
     //Let the vectors be represented as [x,y], with possible values -1,1. 1 == down/right, -1 == left/up
@@ -35,5 +36,23 @@ public class Direction {
             currDirection[0] = 0;
         }
         return currDirection;
+    }
+    public static char getDirection(Maze theMaze, int[] currDirection, int[] position) {
+        char newDirec;
+        int[] right = changeDirection(new int[]{currDirection[0],currDirection[1]}, 'R');
+        int[] left = changeDirection(new int[]{currDirection[0],currDirection[1]}, 'L');
+        if (Objects.equals(theMaze.getTileValue(theMaze.add(position, right)), "P")){//if the right position is open, travel there.
+            newDirec = 'R';
+            currDirection = right;
+        }else if (Objects.equals(theMaze.getTileValue(theMaze.add(position, currDirection)), "P")){//else continue forwards
+            newDirec = 'F';
+        }else if(Objects.equals(theMaze.getTileValue(theMaze.add(position, left)),"P")){//else go left
+            newDirec = 'R';
+            currDirection = left;
+        }else{//else dead end, reverse direction
+            newDirec = 'R';
+            currDirection = right;
+        }
+        return newDirec;
     }
 }
